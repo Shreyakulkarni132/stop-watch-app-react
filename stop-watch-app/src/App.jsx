25fd9edaccf11c9166c4a25cb94af3ade65a58c6
Shreya Kulkarni <shreya.kulkarni132@gmail.com>
2025-03-08 23:18:11 +0530

    first

---
+++ stop-watch-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Stopwatch from './stopwatch.jsx';
+
+function App() {
+
+  return (
+    <>
+
+      <Stopwatch />
+
+    </>
+  )
+}
+
+export default App
